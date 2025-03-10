52b5581b5ad76c98e49bf8dfb5722a3537b95c47
Nath <nathlej41@gmail.com>
2025-03-10 17:43:05 +0100

    first commit

---
+++ src/app/components/NavBar.tsx
@@ -0,0 +1,26 @@
+// import React from 'react';
+// import CategoryFilter from './CategoryFilter';
+
+
+// const Generation = ["test"];
+// const NavBar = () => {
+//     return (
+//         <>
+//             <CategoryFilter categories={Generation} />
+//             <select>Type</select>
+//             <div></div>
+
+//         </>
+//     );
+// }
+// export default NavBar;
+// ○  Filtres par liste déroulante (un seul choix par liste) :
+// ■  Génération
+// ■  Type
+// ○  Tri (croissant/décroissant) :
+// ■  Numéro
+// ■  Alphabétique
+// ■  Poids
+// ■  Taille
+// ○  Recherche par nom (au fur et à mesure que l’utilisateur saisit, la liste est filtrée avec les
+// pokémons dont le nom contient le texte saisi)
---
+++ src/app/context/DataContext.tsx
@@ -0,0 +1,7 @@
+// import React, { createContext, useState, ReactNode, useEffect } from 'react';
+
+// const categories: Array<Category> = [
+//     { id: 1, label: 'Urgent', color: 'red' },
+//     { id: 2, label: 'todo', color: 'blue' },
+//     { id: 3, label: 'when possible', color: 'green' }
+// ];
